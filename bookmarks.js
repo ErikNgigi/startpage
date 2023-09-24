49de846c2f24263eb5832bad05af08a372edc9c1
ErikNgigi <ericmosesngigi@gmail.com>
2023-09-24 11:45:33 +0300

    Custom startpage design for chrome or mozilla browser

---
+++ bookmarks.js
@@ -0,0 +1,61 @@
+const bookmarks = [
+  {
+    title: "Resources",
+    links: [
+      { name: "My Website", url: "https://ericngigi.com" },
+      { name: "ChatGpt", url: "https://chat.openai.com/" },
+      { name: "OdinProject(TOP)", url: "https://www.theodinproject.com/dashboard" },
+      
+      { name: "Linkedin", url: "https://www.linkedin.com/in/eric-ngigi-073b72153/" },
+      { name: "AfricaLeaders(ALX)", url: "https://member.theroom.com/alx/" },
+      { name: "W3Schools", url: "https://my-learning.w3schools.com/" },
+      { name: "HackerRank", url: "https://www.hackerrank.com/" },
+      { name: "RealPython", url: "https://realpython.com/" },
+      { name: "AutomateTheBoringStuff", url: "https://automatetheboringstuff.com/" },
+      { name: "JavaScriptInfo", url: "https://javascript.info/" },
+    ],
+  },
+  {
+    title: "Entertainment",
+    links: [
+      { name: "YouTube", url: "https://youtube.com" },
+      { name: "Pahe", url: "https://pahe.li" },
+      { name: "Nyaa(Torrent)", url: "https://nyaa.si/" },
+      { name: "GogoAnime", url: "https://gogoanime.cl/" },
+      { name: "Piratebay(Torrent)", url: "https://thepiratebay.org/index.html" },
+      { name: "1337x(Torrent)", url: "https://1337x.to/" },
+      { name: "Animelool(index)", url: "https://animelool.com/Anime/" },
+    ],
+  },
+  {
+    title: "Productivity",
+    links: [
+      { name: "ArchWiki", url: "https://wiki.archlinux.org/" },
+      { name: "EricWanyoike", url: "https://mail.google.com/mail/u/?authuser=ericwanyoike@gmail.com" },
+      { name: "EricMosesNgigi", url: "https://mail.google.com/mail/u/?authuser=ericmosesngigi@gmail.com" },
+      { name: "EricWanyoike772", url: "https://mail.google.com/mail/u/?authuser=ericwanyoike772@gmail.com" },
+      { name: "EricTheGiggs", url: "https://mail.google.com/mail/u/?authuser=ericthegiggs@gmail.com" },
+      { name: "Google Drive", url: "https://drive.google.com/drive/u/0/my-drive" },
+      { name: "Google Docs", url: "https://docs.google.com/document/u/0/" },
+      { name: "Google Sheets", url: "https://docs.google.com/spreadsheets/u/0/" },
+      { name: "GitHub", url: "https://github.com/ErikNgigi" },
+    ],
+  },
+  // {
+  //   title: "GMail",
+  //   links: [
+  //     { name: "ericwanyoike", url: "https://mail.google.com/mail/u/?authuser=ericwanyoike@gmail.com" },
+  //     { name: "ericmosesngigi", url: "https://mail.google.com/mail/u/?authuser=ericmosesngigi@gmail.com" },
+  //     { name: "ericwanyoike772", url: "https://mail.google.com/mail/u/?authuser=ericwanyoike772@gmail.com" },
+  //     { name: "ericthegiggs", url: "https://mail.google.com/mail/u/?authuser=ericthegiggs@gmail.com" },
+  //   ],
+  // },
+  // {
+  //   title: "Local",
+  //   links: [
+  //     { name: "hugo", url: "http://localhost:1313" },
+  //     { name: "algoendeavours", url: "http://localhost:8080/algoendeavours/" },
+  //     { name: "phpmyadmin", url: "http://localhost:8080/phpmyadmin/" },
+  //   ],
+  // },
+  ];
