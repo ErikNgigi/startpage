79b0deffd618f90a41d7a5362c18195a98a9de2b
ErikNgigi <ericmosesngigi@gmail.com>
2023-10-10 11:10:26 +0300

    font change

--- bookmarks.js
+++ bookmarks.js
@@ -1,27 +1,24 @@
 const bookmarks = [
   {
-    title: "Resources",
+    title: "Work",
     links: [
-      { name: "My Website", url: "https://ericngigi.com" },
-      { name: "ChatGpt", url: "https://chat.openai.com/" },
-      { name: "OdinProject(TOP)", url: "https://www.theodinproject.com/dashboard" },
-      
-      { name: "Linkedin", url: "https://www.linkedin.com/in/eric-ngigi-073b72153/" },
-      { name: "AfricaLeaders(ALX)", url: "https://member.theroom.com/alx/" },
-      { name: "W3Schools", url: "https://my-learning.w3schools.com/" },
-      { name: "HackerRank", url: "https://www.hackerrank.com/" },
-      { name: "RealPython", url: "https://realpython.com/" },
-      { name: "AutomateTheBoringStuff", url: "https://automatetheboringstuff.com/" },
-      { name: "JavaScriptInfo", url: "https://javascript.info/" },
+      { name: "EricNgigi(website)", url: "https://ericngigi.com" },
+      { name: "Linkedin(ErikNgigi)", url: "https://www.linkedin.com/in/eric-ngigi-073b72153/" },
+      { name: "GitHub(ErikNgigi)", url: "https://github.com/ErikNgigi" },
+      { name: "Netlify(ErikNgigi)", url: "https://app.netlify.com/teams/erikngigi/overview" },
+      { name: "Hugo(Local)", url: "http://localhost:1313" },
+      { name: "AWS(DrawIO)", url: "https://app.diagrams.net/?splash=0&libs=aws4" },
+      { name: "AWS(Certification)", url: "https://www.aws.training/Certification" },
+      { name: "AWS(SkillBuilder)", url: "https://explore.skillbuilder.aws/pages/16/learner-dashboard" },
     ],
   },
   {
-    title: "Entertainment",
+    title: "Personal Interest",
     links: [
       { name: "YouTube", url: "https://youtube.com" },
       { name: "Pahe", url: "https://pahe.li" },
       { name: "Nyaa(Torrent)", url: "https://nyaa.si/" },
-      { name: "GogoAnime", url: "https://gogoanime.cl/" },
+      { name: "GogoAnime", url: "https://gogoanimehd.io/" },
       { name: "Piratebay(Torrent)", url: "https://thepiratebay.org/index.html" },
       { name: "1337x(Torrent)", url: "https://1337x.to/" },
       { name: "Animelool(index)", url: "https://animelool.com/Anime/" },
@@ -30,7 +27,6 @@ const bookmarks = [
   {
     title: "Productivity",
     links: [
-      { name: "ArchWiki", url: "https://wiki.archlinux.org/" },
       { name: "EricWanyoike", url: "https://mail.google.com/mail/u/?authuser=ericwanyoike@gmail.com" },
       { name: "EricMosesNgigi", url: "https://mail.google.com/mail/u/?authuser=ericmosesngigi@gmail.com" },
       { name: "EricWanyoike772", url: "https://mail.google.com/mail/u/?authuser=ericwanyoike772@gmail.com" },
@@ -41,21 +37,19 @@ const bookmarks = [
       { name: "GitHub", url: "https://github.com/ErikNgigi" },
     ],
   },
-  // {
-  //   title: "GMail",
-  //   links: [
-  //     { name: "ericwanyoike", url: "https://mail.google.com/mail/u/?authuser=ericwanyoike@gmail.com" },
-  //     { name: "ericmosesngigi", url: "https://mail.google.com/mail/u/?authuser=ericmosesngigi@gmail.com" },
-  //     { name: "ericwanyoike772", url: "https://mail.google.com/mail/u/?authuser=ericwanyoike772@gmail.com" },
-  //     { name: "ericthegiggs", url: "https://mail.google.com/mail/u/?authuser=ericthegiggs@gmail.com" },
-  //   ],
-  // },
-  // {
-  //   title: "Local",
-  //   links: [
-  //     { name: "hugo", url: "http://localhost:1313" },
-  //     { name: "algoendeavours", url: "http://localhost:8080/algoendeavours/" },
-  //     { name: "phpmyadmin", url: "http://localhost:8080/phpmyadmin/" },
-  //   ],
-  // },
-  ];
+  {
+    title: "Resources",
+    links: [
+      { name: "ChatGpt", url: "https://chat.openai.com/" },
+      { name: "ArchWiki", url: "https://wiki.archlinux.org/" },
+      { name: "OdinProject(TOP)", url: "https://www.theodinproject.com/dashboard" }, 
+      { name: "AfricaLeaders(ALX)", url: "https://member.theroom.com/alx/" },
+      { name: "W3Schools", url: "https://my-learning.w3schools.com/" },
+      { name: "HackerRank", url: "https://www.hackerrank.com/" },
+      { name: "RealPython", url: "https://realpython.com/" },
+      { name: "ATBS(Python)", url: "https://automatetheboringstuff.com/" },
+      { name: "JavaScriptInfo", url: "https://javascript.info/" },
+      { name: "Cpanel", url: "https://exams.cpanel.net/dashboard" },
+    ],
+  },
+];
